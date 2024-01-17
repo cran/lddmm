1f9ee65044b0c6d97a7a266f2cf33214d0c4141a
Giorgio Paulon <giorgio.paulon@utexas.edu>
2024-01-17 20:10:02 +0000

    version 0.4.2

--- inst/doc/minimal_example.R
+++ inst/doc/minimal_example.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -14,38 +14,38 @@ library(latex2exp)
 library(lddmm)
 
 theme_set(theme_bw(base_size = 14))
-cols <- brewer.pal(9, "Set1")
+cols = brewer.pal(9, "Set1")
 
-## ---- eval = TRUE, results = 'hide', fig.show = 'hide', warning = FALSE, message = FALSE----
-# Load the data
-data('data')
-
-# Descriptive plots
-plot_accuracy(data)
-plot_RT(data)
-
-# Run the model
-hypers <- NULL
-hypers$s_sigma_mu <- hypers$s_sigma_b <- 0.1
-
-# Change the number of iterations when running the model
-# Here the number is small so that the code can run in less than 1 minute
-Niter <- 25
-burnin <- 15
-thin <- 1
-samp_size <- (Niter - burnin) / thin
-
-set.seed(123)
-fit <- LDDMM(data = data, 
-             hypers = hypers, 
-             Niter = Niter, 
-             burnin = burnin, 
-             thin = thin)
-
-# Plot the results
-plot_post_pars(data, fit, par = 'drift')
-plot_post_pars(data, fit, par = 'boundary')
-
-# Compute the WAIC to compare models
-compute_WAIC(fit)
+## ----eval = FALSE, results = 'hide', fig.show = 'hide', warning = FALSE, message = FALSE----
+#  # Load the data
+#  data('data')
+#  
+#  # Descriptive plots
+#  plot_accuracy(data)
+#  plot_RT(data)
+#  
+#  # Run the model
+#  hypers = NULL
+#  hypers$s_sigma_mu = hypers$s_sigma_b = 0.1
+#  
+#  # Change the number of iterations when running the model
+#  # Here the number is small so that the code can run in less than 1 minute
+#  Niter = 25
+#  burnin = 15
+#  thin = 1
+#  samp_size = (Niter - burnin) / thin
+#  
+#  set.seed(123)
+#  fit = LDDMM(data = data,
+#               hypers = hypers,
+#               Niter = Niter,
+#               burnin = burnin,
+#               thin = thin)
+#  
+#  # Plot the results
+#  plot_post_pars(data, fit, par = 'drift')
+#  plot_post_pars(data, fit, par = 'boundary')
+#  
+#  # Compute the WAIC to compare models
+#  compute_WAIC(fit)
 
